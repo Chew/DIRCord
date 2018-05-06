5de753b97cd5d52e173f65d7e0ae73229217d6bb
Chewsterchew <chew@chew.pw>
2018-05-06 13:49:20 -0500

    Gems needed to run the program yummer

---
+++ requiregems.rb
@@ -0,0 +1,65 @@
+begin
+  require 'cinch'
+rescue LoadError
+  puts "You're missing the gem `cinch`. Would you like to install this now? (y/n)"
+  input = gets.chomp
+  if input == 'y'
+    `gem install cinch`
+    puts 'Gem installed! Continuing..'
+  else
+    puts 'To continue, install the cinch gem'
+    exit
+  end
+end
+begin
+  require 'discordrb'
+rescue LoadError
+  puts "You're missing the gem `discordrb`. Would you like to install this now? (y/n)"
+  input = gets.chomp
+  if input == 'y'
+    `gem install cinch`
+    puts 'Gem installed! Continuing..'
+  else
+    puts 'To continue, install the cinch gem'
+    exit
+  end
+end
+begin
+  require 'rest-client'
+rescue LoadError
+  puts "You're missing the gem `rest-client`. Would you like to install this now? (y/n)"
+  input = gets.chomp
+  if input == 'y'
+    `gem install rest-client`
+    puts 'Gem installed! Continuing..'
+  else
+    puts 'Without rest-client, commands like !cat, !trbmb, !youtube, and many more will not work! It is recommended to say n, unless you are testing a plugin that does not require rest-client, continue? (y/n)'
+    input = gets.chomp
+    if input == 'n'
+      puts 'To continue, install the rest-client gem'
+      exit
+    end
+  end
+end
+require 'json'
+require 'net/http'
+require 'yaml'
+begin
+  require 'nokogiri'
+rescue LoadError
+  puts "You're missing the gem `nokogiri`. Would you like to install this now? (y/n)"
+  input = gets.chomp
+  if input == 'y'
+    `gem install nokogiri`
+    puts 'Gem installed! Continuing..'
+  else
+    puts 'Without Nokogiri, !cat will not work. Continue? (y/n)'
+    input = gets.chomp
+    if input == 'n'
+      puts 'To continue, install the nokogiri gem'
+      exit
+    end
+  end
+end
+require 'open-uri'
+require 'base64'
