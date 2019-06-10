76cd43ed02e1ab3438a1b972cbd3b6ca9814da15
Chew <chew@chew.pw>
2019-06-09 19:18:38 -0500

    fix ~topic and make the return message more descriptive

--- discord/topic.rb
+++ discord/topic.rb
@@ -5,9 +5,9 @@ module Topic
     modes = []
     m = Irc.Channel("\##{event.channel.name}")
     m.modes.each do |e, _f|
-      modes[modes.length] = e
+      modes.push e
     end
-    event.channel.topic = "[+#{modes.join('')}] #{m.topic.gsub!(/([0-9][0-9]|[0-9])/, '')}"
-    event.respond 'Set the channel topic!'
+    event.channel.topic = "[+#{modes.join('')}] #{m.topic.gsub(/([0-9][0-9]|[0-9])/, '')}"
+    event.respond "Synced this channel's topic!"
   end
 end
