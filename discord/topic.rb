9be5a78d7321cd080f39be09c0a1856401d007c5
Saorn <jackrobertson.USA@gmail.com>
2019-06-18 16:57:35 -0700

    Update discord/topic.rb
    Removed extra space.
    
    Co-Authored-By: Chew <chew@chew.pw>

--- discord/topic.rb
+++ discord/topic.rb
@@ -1,7 +1,7 @@
 module Topic
   extend Discordrb::Commands::CommandContainer
 
-  command(:topic, description: "Update's discord channel's topic to match that of the connected IRC channel.  Also displays channel modes.") do |event|
+  command(:topic, description: "Update's discord channel's topic to match that of the connected IRC channel. Also displays channel modes.") do |event|
     modes = []
     m = Irc.Channel("\##{event.channel.name}")
     m.modes.each do |e, _f|
