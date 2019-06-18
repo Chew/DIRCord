5ec026006e5f98310fb5d7c6a9eb63dfdfa0b402
Saorn <soarn@soarn.pro>
2019-06-18 16:42:16 -0700

    Update highlights.rb
    Added better description.

--- discord/highlights.rb
+++ discord/highlights.rb
@@ -1,7 +1,7 @@
 module Highlights
   extend Discordrb::Commands::CommandContainer
 
-  command(:highlights, min_args: 1, max_args: 2, description: "Marks a phrase as alertable, and will alert you when that phrase is used.") do |event, action, word|
+  command(:highlights, min_args: 1, max_args: 2, description: "Use `list` to view alertable phrases you have set up, use `add` and `remove` to add or remove alertable phrases respectively.") do |event, action, word|
     if CONFIG['highlights'].nil?
       CONFIG['highlights'] = ''
       File.open(CONFIG_FILE, 'w') { |f| f.write CONFIG.to_yaml }
