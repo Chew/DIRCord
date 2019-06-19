29d552c0bd91a1ca2074383561e80bbdc640c690
Saorn <soarn@soarn.pro>
2019-06-18 17:03:03 -0700

    Update eval.rb
    Added more descriptive description.

--- discord/eval.rb
+++ discord/eval.rb
@@ -1,7 +1,7 @@
 module Eval
   extend Discordrb::Commands::CommandContainer
 
-  command(:eval, description: "Evals given input through the bot.") do |event, *code|
+  command(:eval, description: "Evaluates Ruby expressions.  For Ruby experts, use with caution") do |event, *code|
     break unless event.user.id == CONFIG['user_id']
 
     begin
